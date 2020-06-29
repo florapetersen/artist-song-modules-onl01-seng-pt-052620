289085e8b75d09d1e0fb03bfc14f4ff6833f1ab6
Flora Petersen <floramarina@comcast.net>
2020-06-29 19:12:23 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,6 +1,7 @@
 module Findable
   module ClassMethods
-  def self.find_by_name(name)
-    self.all.detect{|a| a.name == name}
+    def self.find_by_name(name)
+      self.all.detect{|a| a.name == name}
+    end
   end
 end
